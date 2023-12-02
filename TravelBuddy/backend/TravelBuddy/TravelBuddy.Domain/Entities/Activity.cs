41330f0037d509e82017d3ebeef8d68e843206bf
Simeon Yordanov <sjordanov@code.bg>
2023-12-02 23:24:31 +0200

    minor commit

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Domain/Entities/Activity.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelBuddy.Domain.Entities
+{
+	public class Activity
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string Description { get; set; } = null!;
+
+		public int ItineraryId { get; set; }
+		public Itinerary Itinerary { get; set; } = null!;
+	}
+}
