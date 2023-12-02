ddf0c7d3a604e88b473426a73fb580882dc6c0d7
Simeon Yordanov <sjordanov@code.bg>
2023-12-02 23:26:23 +0200

    created Configuration files for all Entites

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/EntitiyConfigurations/ItinerariesEntityTypeConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelBuddy.Infrastructure.EntitiyConfigurations
+{
+	internal class ItinerariesEntityTypeConfiguration : IEntityTypeConfiguration<Itinerary>
+	{
+		public void Configure(EntityTypeBuilder<Itinerary> builder)
+		{
+			builder
+				.HasKey(x => x.Id);
+
+		}
+	}
+}
---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/EntitiyConfigurations/UsersEntityTypeConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelBuddy.Infrastructure.EntitiyConfigurations
+{
+	internal class UsersEntityTypeConfiguration : IEntityTypeConfiguration<User>
+	{
+		public void Configure(EntityTypeBuilder<User> builder)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
