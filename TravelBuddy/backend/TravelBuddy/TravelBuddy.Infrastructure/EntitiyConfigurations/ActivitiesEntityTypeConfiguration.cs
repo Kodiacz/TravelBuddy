dfe413e51f33fe99ff145840b8959fb221891509
Simeon Yordanov <sjordanov@code.bg>
2023-12-02 23:26:50 +0200

    implemented configuration for Activities

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/EntitiyConfigurations/ActivitiesEntityTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace TravelBuddy.Infrastructure.EntitiyConfigurations
+{
+	internal class ActivitiesEntityTypeConfiguration : IEntityTypeConfiguration<Activity>
+	{
+		public void Configure(EntityTypeBuilder<Activity> builder)
+		{
+			builder
+				.HasKey(x => x.Id);
+
+			builder
+				.HasOne(x => x.Itinerary)
+				.WithMany(x => x.Activities)
+				.HasForeignKey(x => x.ItineraryId);
+
+			builder
+				.Property(x => x.Name)
+				.IsRequired()
+				.HasMaxLength(ACTIVITY_NAME_MAX_LENGTH);
+
+			builder
+				.Property (x => x.Description)
+				.HasMaxLength(ACTIVITY_DESCRIPTION_MAX_LENGTH);
+
+			builder
+				.Property(x => x.ItineraryId)
+				.IsRequired();
+		}
+	}
+}
