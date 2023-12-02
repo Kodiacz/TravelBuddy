5db8e67576a0ce6cbe0ed60c1cfb1ff2a7fb14bf
Simeon Yordanov <sjordanov@code.bg>
2023-12-02 10:55:44 +0200

    created Db context

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/TravelBuddyDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace TravelBuddy.Infrastructure
+{
+	public class TravelBuddyDbContext : DbContext
+	{
+        public TravelBuddyDbContext(DbContextOptions<TravelBuddyDbContext> options) : base(options) {}
+
+		public DbSet<User> Users { get; set; } = null!;
+
+		public DbSet<Itinerary> Itineraries { get; set; } = null!;
+
+		public DbSet<Trip> Trips { get; set; } = null!;
+
+		public DbSet<Activities> Activities { get; set; } = null!;
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			//TODO: Aplly Configurtations
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
