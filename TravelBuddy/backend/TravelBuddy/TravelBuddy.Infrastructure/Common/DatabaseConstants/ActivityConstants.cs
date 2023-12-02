6187765d0e6860994b70ad83bc83e14476d3f877
Simeon Yordanov <sjordanov@code.bg>
2023-12-02 23:25:52 +0200

    created constants for Activity Entity

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/Common/DatabaseConstants/ActivityConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelBuddy.Infrastructure.Common.DatabaseConstants
+{
+	public class ActivityConstants
+	{
+		public const int ACTIVITY_NAME_MAX_LENGTH = 40;
+
+		public const int ACTIVITY_DESCRIPTION_MAX_LENGTH = 500;
+	}
+}
