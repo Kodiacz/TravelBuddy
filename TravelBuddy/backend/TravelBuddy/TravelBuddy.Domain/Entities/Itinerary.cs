a1a343fd1a763a6bfe37a54c3ff7d4b6190e1832
Simeon Yordanov <sjordanov@code.bg>
2023-12-04 00:01:28 +0200

    added trip property

--- TravelBuddy/backend/TravelBuddy/TravelBuddy.Domain/Entities/Itinerary.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Domain/Entities/Itinerary.cs
@@ -11,7 +11,10 @@
 
 		public string Name { get; set; } = null!;
 
-		public DateTime Date { get; set; }
+		public DateTime Date { get; set; }	
+
+		public int TripId { get; set; }
+		public Trip Trip { get; set; } = null!;
 
 		public ICollection<Activity> Activities { get; set; }
 	}
