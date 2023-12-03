458c5f04afa479eafe948a3a2b452e4d74d9a604
Simeon Yordanov <sjordanov@code.bg>
2023-12-04 00:03:04 +0200

    added new global usings

--- TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/GlobalUsings.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/GlobalUsings.cs
@@ -3,3 +3,5 @@ global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 global using TravelBuddy.Domain.Entities;
 global using static TravelBuddy.Infrastructure.Common.DatabaseConstants.ActivityConstants;
+global using static TravelBuddy.Infrastructure.Common.DatabaseConstants.ItineraryConstants;
+global using static TravelBuddy.Infrastructure.Common.DatabaseConstants.TripConstants;
