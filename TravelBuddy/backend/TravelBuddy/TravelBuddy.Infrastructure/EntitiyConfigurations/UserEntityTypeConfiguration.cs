9b732a2c7762752bafc7912915ce9f1a315de0fb
Simeon Yordanov <sjordanov@code.bg>
2023-12-14 20:59:21 +0200

    added User entity type configurations

--- TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/EntitiyConfigurations/UserEntityTypeConfiguration.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/EntitiyConfigurations/UserEntityTypeConfiguration.cs
@@ -9,7 +9,31 @@
 
 			builder
 				.Property(user => user.FirstName)
+				.HasMaxLength(USER_FIRST_NAME_MAX_LENGTH)
 				.IsRequired();
+
+			builder
+			.Property(user => user.LastName)
+				.HasMaxLength(USER_LAST_NAME_MAX_LENGTH)
+			.IsRequired();
+
+			builder
+				.Property(user => user.UserName)
+				.HasMaxLength(USER_NAME_MAX_LENGTH)
+				.IsRequired();
+
+			builder
+			.Property(user => user.Email)
+				.HasMaxLength(USER_EMAIL_MAX_LENGTH)
+			.IsRequired();
+
+			builder
+				.Property(user => user.Password)
+				.IsRequired();
+
+			builder
+				.HasCheckConstraint("CK_User_Created", $"{nameof(User.Created)} >= {DateTime.UtcNow}");
+
 		}
 	}
 }
