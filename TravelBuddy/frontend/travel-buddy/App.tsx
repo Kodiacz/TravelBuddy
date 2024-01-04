a1b0c87364a7aa5ca3e2c0877f4922a6e7924c2f
Simeon Yordanov <sjordanov@code.bg>
2024-01-04 10:46:28 +0200

    added store to App

--- TravelBuddy/frontend/travel-buddy/App.tsx
+++ TravelBuddy/frontend/travel-buddy/App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomePage from "./src/components/HomePage";
+import { AppState } from "./src/redux/store";
+import {
+  incrementCount,
+  decrementCount,
+} from "./src/redux/counter/CounterAction";
 
-export default function App() {
+import { Provider, connect } from "react-redux";
+import { Dispatch } from "redux";
+import { store } from "./src/redux/store";
+
+interface AppProps {
+  increment: () => void;
+  decrement: () => void;
+}
+
+const mapStateToProps = (state: AppState) => ({
+  count: state.counterReducer.count,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch): AppProps => ({
+  increment: () => dispatch(incrementCount()),
+  decrement: () => dispatch(decrementCount()),
+});
+
+export default function App<AppProps, AppState>(props: AppState) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <HomePage />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
+
+connect(mapStateToProps, mapDispatchToProps)(App);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
