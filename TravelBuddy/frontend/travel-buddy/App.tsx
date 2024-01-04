d3f049d79411c90e3c737ee87219e8bb23371fd2
Simeon Yordanov <sjordanov@code.bg>
2024-01-05 01:20:26 +0200

    added and configured navigation and SafeArea as well

--- TravelBuddy/frontend/travel-buddy/App.tsx
+++ TravelBuddy/frontend/travel-buddy/App.tsx
@@ -1,48 +1,59 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import HomePage from "./src/components/HomePage";
-import { AppState } from "./src/redux/store";
+import LandingScreen from './src/screens/Landing';
+import RegisterScreen from './src/screens/authentication/Register';
+import { AppState } from './src/redux/store';
 import {
-  incrementCount,
-  decrementCount,
-} from "./src/redux/counter/CounterAction";
+	incrementCount,
+	decrementCount,
+} from './src/redux/counter/CounterAction';
 
-import { Provider, connect } from "react-redux";
-import { Dispatch } from "redux";
-import { store } from "./src/redux/store";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider, connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { store } from './src/redux/store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList } from './src/types/types';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 interface AppProps {
-  increment: () => void;
-  decrement: () => void;
+	increment: () => void;
+	decrement: () => void;
 }
 
 const mapStateToProps = (state: AppState) => ({
-  count: state.counterReducer.count,
+	count: state.counterReducer.count,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch): AppProps => ({
-  increment: () => dispatch(incrementCount()),
-  decrement: () => dispatch(decrementCount()),
+	increment: () => dispatch(incrementCount()),
+	decrement: () => dispatch(decrementCount()),
 });
 
-export default function App<AppProps, AppState>(props: AppState) {
-  return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <HomePage />
-        <StatusBar style="auto" />
-      </View>
-    </Provider>
-  );
+export default function App() {
+	return (
+		<SafeAreaProvider>
+			<Provider store={store}>
+				<NavigationContainer>
+					<Stack.Navigator
+						initialRouteName="Landing"
+						screenOptions={{
+							header: () => null,
+						}}
+					>
+						<Stack.Screen
+							name="Landing"
+							component={LandingScreen}
+						/>
+						<Stack.Screen
+							name="Register"
+							component={RegisterScreen}
+						/>
+					</Stack.Navigator>
+				</NavigationContainer>
+			</Provider>
+		</SafeAreaProvider>
+	);
 }
 
 connect(mapStateToProps, mapDispatchToProps)(App);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
