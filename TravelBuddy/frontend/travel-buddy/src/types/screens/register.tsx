8ed24acc6efbe5c19c4254a5e0da4e1ca5e6d71a
Simeon Yordanov <sjordanov@code.bg>
2024-01-05 09:53:47 +0200

    created types

---
+++ TravelBuddy/frontend/travel-buddy/src/types/screens/register.tsx
@@ -0,0 +1,5 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types';
+
+export interface IRegisterProps
+	extends NativeStackScreenProps<RootStackParamList> {}
