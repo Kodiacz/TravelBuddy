9c9babc6f99906f47b81a5853cc39b22ef1d2cf0
Simeon Yordanov <sjordanov@code.bg>
2024-01-06 19:12:53 +0200

    created ApplicationUserDto

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Dtos/ApplicationUserDto/CreateApplicationUserDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelBuddy.Application.Dtos.ApplicationUserDto
+{
+	public class CreateApplicationUserDto
+	{
+		[Required]
+
+		public string FirstName { get; set; } = null!;
+
+		[Required]
+		public string LastName { get; set; } = null!;
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; } = null!;
+
+		[Required]
+		[PasswordPropertyText]
+		public string Password { get; set; } = null!;
+
+		[Required]
+		[PasswordPropertyText]
+		public string ConfirmPassword { get; set; } = null!;
+
+	}
+}
