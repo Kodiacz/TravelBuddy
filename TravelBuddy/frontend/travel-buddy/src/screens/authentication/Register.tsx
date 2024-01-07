90cad8605e993a60c967b481adb9a2cea5cad05a
Simeon Yordanov <sjordanov@code.bg>
2024-01-07 10:16:31 +0200

    added custom input fields and added style for buttons when they are pressed

--- TravelBuddy/frontend/travel-buddy/src/screens/authentication/Register.tsx
+++ TravelBuddy/frontend/travel-buddy/src/screens/authentication/Register.tsx
@@ -1,8 +1,18 @@
-import React, { ChangeEvent, useState } from 'react';
-import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import {
+	Pressable,
+	StyleSheet,
+	Text,
+	View,
+	KeyboardAvoidingView,
+	Platform,
+} from 'react-native';
 import { IRegisterProps } from '../../types/screens/register';
 import useSafeArea from '../../custom-hooks/useSafeView';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import InputField from '../../components/InputField';
+import { colors } from '../../utils/colors';
+import { Dimensions } from 'react-native';
 
 interface IRegisterData {
 	firstName: string;
@@ -11,6 +21,8 @@ interface IRegisterData {
 	password: string;
 	confirmPassword: string;
 }
+
+const screenWidth = Dimensions.get('window').width;
 
 type inputFieldName =
 	| 'firstName'
@@ -26,75 +38,125 @@ export default function Register({ navigation }: IRegisterProps) {
 		control,
 		handleSubmit,
 		formState: { errors },
-	} = useForm();
+		getValues,
+	} = useForm<IRegisterData>();
 	const viewContainerStyle = { ...safeArea, ...styles.container };
 
-	const handleInput = (input: string, fieldName: inputFieldName) => {
-		console.log('prev state => ', user);
-		setUser((prev) => {
-			return { ...prev!, [fieldName]: input };
-		});
+	const passwordMatchValidation = (value: string, values: IRegisterData) => {
+		const confirmPassword = values.confirmPassword;
+		return confirmPassword === value || 'Passwords do not match';
+	};
 
-		console.log('new state => ', user);
+	const onSubmit = (data: any) => {
+		console.log(data);
 	};
 
 	return (
-		<View style={viewContainerStyle}>
-			<View style={styles.inputsContainer}>
-				<Controller
-					control={control}
-					name="username"
-					defaultValue=""
-					rules={{
-						required: { value: true, message: "Nom d'utilisateur nécessaire" },
-					}}
-					render={({ field: { onChange, value } }) => (
-						<>
-							<TextInput
-								underlineColorAndroid="transparent"
-								style={styles.inputField}
-								placeholder="First Name"
-								placeholderTextColor="#0C2668"
-								onChangeText={(text) => handleInput(text, 'firstName')}
-							/>
-							<TextInput
-								underlineColorAndroid="transparent"
-								style={styles.inputField}
-								placeholder="Last Name"
-								placeholderTextColor="#0C2668"
-								onChangeText={(text) => handleInput(text, 'lastName')}
-							/>
-							<TextInput
-								underlineColorAndroid={'transparent'}
-								style={styles.inputField}
-								placeholder="Email"
-								placeholderTextColor="#0C2668"
-								onChangeText={(text) => handleInput(text, 'email')}
-							/>
-							<TextInput
-								underlineColorAndroid="transparent"
-								style={styles.inputField}
-								placeholder="Password"
-								placeholderTextColor="#0C2668"
-								onChangeText={(text) => handleInput(text, 'password')}
-							/>
-							<TextInput
-								underlineColorAndroid="transparent"
-								style={styles.inputField}
-								placeholder="Confirm Password"
-								placeholderTextColor="#0C2668"
-								onChangeText={(text) => handleInput(text, 'confirmPassword')}
-							/>
-						</>
-					)}
-				/>
-				<View style={styles.buttonsContainer}>
-					<Pressable style={styles.button}>
-						<Text style={styles.buttonTetxt}>REGISTER</Text>
-					</Pressable>
+		<KeyboardAvoidingView
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+			style={{ flex: 1 }}
+		>
+			<View style={viewContainerStyle}>
+				<View style={styles.inputsContainer}>
+					<InputField
+						control={control}
+						placeholder="First Name"
+						placeholderTextColor={colors.primary.fibonacciBlue}
+						name="firstName"
+						textInputStyle={
+							errors.firstName
+								? { ...styles.inputField, ...styles.inputFieldRedBoreder }
+								: styles.inputField
+						}
+						errorTextStyle={styles.errorText}
+						rules={{ required: 'First Name is required' }}
+						error={errors.firstName?.message?.toString()}
+					/>
+					<InputField
+						control={control}
+						name="lastName"
+						placeholder="Last Name"
+						placeholderTextColor={colors.primary.fibonacciBlue}
+						textInputStyle={
+							errors.lastName
+								? { ...styles.inputField, ...styles.inputFieldRedBoreder }
+								: styles.inputField
+						}
+						errorTextStyle={styles.errorText}
+						rules={{ required: 'Last Name is required' }}
+						error={errors.lastName?.message?.toString()}
+					/>
+					<InputField
+						control={control}
+						name="email"
+						placeholder="Email"
+						keyboardType="email-address"
+						inputFieldType="emailAddress"
+						placeholderTextColor={colors.primary.fibonacciBlue}
+						textInputStyle={
+							errors.email
+								? { ...styles.inputField, ...styles.inputFieldRedBoreder }
+								: styles.inputField
+						}
+						errorTextStyle={styles.errorText}
+						rules={{
+							required: 'Email is required',
+						}}
+						error={errors.email?.message?.toString()}
+					/>
+					<InputField
+						control={control}
+						name="password"
+						placeholder="Password"
+						secureTextEntry={true}
+						placeholderTextColor={colors.primary.fibonacciBlue}
+						inputFieldType="password"
+						textInputStyle={
+							errors.password
+								? { ...styles.inputField, ...styles.inputFieldRedBoreder }
+								: styles.inputField
+						}
+						errorTextStyle={styles.errorText}
+						rules={{
+							required: 'Password is required',
+							validate: (value) => passwordMatchValidation(value, getValues()),
+						}}
+						error={errors.password?.message?.toString()}
+					/>
+					<InputField
+						control={control}
+						secureTextEntry={true}
+						name="confirmPassword"
+						placeholder="Confirm Password"
+						inputFieldType="password"
+						placeholderTextColor={colors.primary.fibonacciBlue}
+						textInputStyle={
+							errors.confirmPassword
+								? [styles.inputField, styles.inputFieldRedBoreder]
+								: styles.inputField
+						}
+						errorTextStyle={styles.errorText}
+						rules={{
+							required: 'Confirm Password is required',
+							validate: (value) => passwordMatchValidation(value, getValues()),
+						}}
+						error={errors.confirmPassword?.message?.toString()}
+					/>
+					<View style={styles.buttonsContainer}>
+						<Pressable
+							// style={styles.button}
+							style={({ pressed }) => [
+								styles.button,
+								pressed && styles.buttonPressed,
+							]}
+							onPress={handleSubmit(onSubmit)}
+						>
+							<Text style={styles.buttonTetxt}>REGISTER</Text>
+						</Pressable>
+					</View>
 				</View>
 			</View>
-		</View>
+		</KeyboardAvoidingView>
 	);
 }
 
@@ -110,6 +172,7 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 	},
 	inputsContainer: {
+		width: screenWidth * 0.9,
 		paddingTop: 24,
 		paddingRight: 16,
 		paddingLeft: 16,
@@ -120,9 +183,8 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		justifyContent: 'flex-end',
 	},
-	// input field
 	inputField: {
-		width: 330,
+		width: 'auto',
 		height: 41.51,
 		borderRadius: 11.19,
 		backgroundColor: '#FFFFFF',
@@ -132,9 +194,16 @@ const styles = StyleSheet.create({
 		fontSize: 17,
 		letterSpacing: -0.4,
 		lineHeight: 22,
-		borderBottomWidth: 0,
 		padding: 0,
 		paddingLeft: 14.92,
+	},
+	errorText: {
+		color: colors.error,
+		paddingLeft: 14.92,
+	},
+	inputFieldRedBoreder: {
+		borderColor: colors.error,
+		borderWidth: 1,
 	},
 	button: {
 		width: 'auto',
@@ -148,6 +217,9 @@ const styles = StyleSheet.create({
 		marginTop: 12,
 		marginRight: 8,
 		backgroundColor: '#0C2668',
+	},
+	buttonPressed: {
+		backgroundColor: '#4071C1', // Change to a different color or add any other styles
 	},
 	buttonTetxt: {
 		color: '#FFFFFF',
