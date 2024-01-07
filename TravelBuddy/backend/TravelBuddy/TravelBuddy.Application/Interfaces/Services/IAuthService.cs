a57b2ff9ceb85b21664b85ebc78c6d20710ce15b
Simeon Yordanov <sjordanov@code.bg>
2024-01-07 11:58:43 +0200

    defined and implemented Auth Service

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelBuddy.Application.Interfaces.Services
+{
+	public interface IAuthService
+	{
+		public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+
+		public Task RegisterUser(CreateApplicationUserDto dto);
+	}
+}
---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/Services/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using TravelBuddy.Application.Dtos.ApplicationUserDto;
+
+namespace TravelBuddy.Infrastructure.Services
+{
+	public class AuthService : IAuthService
+	{
+		private readonly IUnitOfWork unitOfWork;
+		private readonly IMapper mapper;
+
+		public AuthService(IUnitOfWork unitOfWork, IMapper mapper)
+		{
+			this.unitOfWork = unitOfWork;
+			this.mapper = mapper;
+		}
+
+		public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+		{
+			using (var hmac = new HMACSHA512())
+			{
+				passwordSalt = hmac.Key;
+				passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+			}
+		}
+
+		public async Task RegisterUser(CreateApplicationUserDto dto) 
+		{
+			ApplicationUser user = mapper.Map<ApplicationUser>(dto);
+			await unitOfWork.ApplicationUserRepository.AddAsync(user);
+		}
+	}
+}
