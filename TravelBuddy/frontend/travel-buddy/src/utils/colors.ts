c7bfca595baf6f6b8d13d4e467762209a561af57
Simeon Yordanov <sjordanov@code.bg>
2024-01-07 10:16:47 +0200

    created colors

---
+++ TravelBuddy/frontend/travel-buddy/src/utils/colors.ts
@@ -0,0 +1,75 @@
+export const colors: ColorsType = {
+	primary: {
+		fibonacciBlue: '#112557',
+		lambentLagoon: '#3C5490',
+		dutchBlue: '#4D6295',
+		orbital: '#6E85BC',
+		skyBlue: '#A1B4E5',
+		frostFairy: '#BFCEF2',
+	},
+	seconday: {
+		blue: {
+			persianJewel: '#737DC2',
+			sorbetIceMauve: '#9EA5D7',
+			powderedGranite: '#C1C7EC',
+			gardenFairy: '#CED3EE',
+			icyLavender: '#E2E3EE',
+			ghostWhite: '#F9FAFF',
+			paleGrey: '#FBFCFF',
+		},
+		grey: {
+			direWolf: '#272727',
+			boatAnchor: '#6B6B6B',
+			moreThanAWeek: '#8C8C8C',
+			discoBall: '#D4D4D4',
+			plaster: '#EAEAEA',
+		},
+	},
+	accent: {
+		pumpking: '#FF7B1B',
+		absoluteApricot: '#FF9447',
+		goGoMango: '#FFB681',
+		karry: '#FFDCC2',
+		sugarMilk: '#FFF8F3',
+	},
+	white: '#FFFFFF',
+	error: '#CC1919',
+};
+
+type ColorsType = {
+	primary: {
+		fibonacciBlue: '#112557';
+		lambentLagoon: '#3C5490';
+		dutchBlue: '#4D6295';
+		orbital: '#6E85BC';
+		skyBlue: '#A1B4E5';
+		frostFairy: '#BFCEF2';
+	};
+	seconday: {
+		blue: {
+			persianJewel: '#737DC2';
+			sorbetIceMauve: '#9EA5D7';
+			powderedGranite: '#C1C7EC';
+			gardenFairy: '#CED3EE';
+			icyLavender: '#E2E3EE';
+			ghostWhite: '#F9FAFF';
+			paleGrey: '#FBFCFF';
+		};
+		grey: {
+			direWolf: '#272727';
+			boatAnchor: '#6B6B6B';
+			moreThanAWeek: '#8C8C8C';
+			discoBall: '#D4D4D4';
+			plaster: '#EAEAEA';
+		};
+	};
+	accent: {
+		pumpking: '#FF7B1B';
+		absoluteApricot: '#FF9447';
+		goGoMango: '#FFB681';
+		karry: '#FFDCC2';
+		sugarMilk: '#FFF8F3';
+	};
+	white: '#FFFFFF';
+	error: '#CC1919';
+};
