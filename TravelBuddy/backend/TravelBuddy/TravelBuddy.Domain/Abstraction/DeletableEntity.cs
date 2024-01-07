babbd7b71a6004c7f88c8c1e4b24cb0442e18ede
Simeon Yordanov <sjordanov@code.bg>
2024-01-07 10:12:30 +0200

    created DeletableEntity

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Domain/Abstraction/DeletableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBuddy.Domain.Abstraction
+{
+	public interface IDeletableEntity
+	{
+		public bool Deleted { get; set; }
+	}
+}
