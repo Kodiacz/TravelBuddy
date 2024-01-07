b8708e131b5272b20cd262aac5c8948160f84816
Simeon Yordanov <sjordanov@code.bg>
2024-01-07 10:13:54 +0200

    changed ApplicationUser

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TravelBuddy.Domain.Abstraction;
+
+namespace TravelBuddy.Domain.Entities
+{
+	public class ApplicationUser : IdentityUser<int>, IDeletableEntity
+	{
+		public ApplicationUser()
+		{
+			this.CreatedTrips = new HashSet<Trip>();
+			this.InvitedToTrips = new HashSet<Trip>();
+		}
+
+		public string FirstName { get; set; } = null!;
+
+		public string LastName { get; set; } = null!;
+
+		public string? ProfileImage { get; set; }
+
+		public byte[]? PasswordSalt { get; set; }
+
+		//public string Password { get; set; } = null!;
+		public ICollection<Trip> CreatedTrips { get; set; }
+
+		/// <summary>
+		/// All trips in which current user is invited
+		/// </summary>
+		public ICollection<Trip> InvitedToTrips { get; set; }
+
+		/// <summary>
+		/// The date on which the user was registered
+		/// </summary>
+		public DateTime Created { get; set; }
+
+		public DateTime LastLoggedIn { get; set; }
+
+		public bool Deleted { get; set; } = false;
+
+		public DateTime DateDeleted { get; set; }
+	}
+}
