74e5dc4120a2f258f10d5b95eac8b9c8b07b04a1
Simeon Yordanov <sjordanov@code.bg>
2024-01-07 10:11:19 +0200

    created dtos

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Dtos/ApplicationUserDto/GetApplicationUserDto.cs
@@ -0,0 +1,29 @@
+﻿namespace TravelBuddy.Application.Dtos.ApplicationUserDto
+{
+	public class GetApplicationUserDto
+	{
+        public GetApplicationUserDto()
+        {
+			this.CreatedTrips = new HashSet<Trip>();  
+			this.InvitedToTrips = new HashSet<Trip>();  
+        }
+
+        public string FirstName { get; set; } = null!;
+
+		public string LastName { get; set; } = null!;
+
+		public string Email { get; set; } = null!;
+
+		public string UserName { get; set; } = null!;
+
+		public string? ProfileImage { get; set; } 
+
+		public ICollection<Trip> CreatedTrips { get; set; }
+
+		public ICollection<Trip> InvitedToTrips { get; set; }
+
+		public DateTime Created { get; set; }
+
+		public DateTime LastLoggedIn { get; set; }
+	}
+}
