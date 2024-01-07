34e64071b9dabff0db932c704d57846d95972bf0
Simeon Yordanov <sjordanov@code.bg>
2024-01-07 10:14:30 +0200

    renamed file

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/EntitiyConfigurations/ApplicationUserEntityTypeConfiguration.cs
@@ -0,0 +1,38 @@
+﻿namespace TravelBuddy.Infrastructure.EntitiyConfigurations
+{
+	internal class ApplicationUserEntityTypeConfiguration : IEntityTypeConfiguration<ApplicationUser>
+	{
+		public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+		{
+			builder
+				.HasKey(user => user.Id);
+
+			builder
+				.Property(user => user.FirstName)
+				.HasMaxLength(USER_FIRST_NAME_MAX_LENGTH)
+				.IsRequired();
+
+			builder
+			.Property(user => user.LastName)
+				.HasMaxLength(USER_LAST_NAME_MAX_LENGTH)
+			.IsRequired();
+
+			builder
+				.Property(user => user.UserName)
+				.HasMaxLength(USER_NAME_MAX_LENGTH)
+				.IsRequired();
+
+			builder
+			.Property(user => user.Email)
+				.HasMaxLength(USER_EMAIL_MAX_LENGTH)
+			.IsRequired();
+
+			builder
+				.Property(user => user.PasswordHash)
+				.IsRequired();
+
+			builder
+				.HasCheckConstraint("CK_User_Created", $"{nameof(ApplicationUser.Created)} >= GETDATE()");
+		}
+	}
+}
