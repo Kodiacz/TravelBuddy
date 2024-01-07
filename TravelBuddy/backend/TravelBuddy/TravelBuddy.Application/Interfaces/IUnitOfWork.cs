280c75a30c57e8473670bf83ba1adef261068e83
Simeon Yordanov <sjordanov@code.bg>
2024-01-07 10:11:46 +0200

    defined and implemented unit of work

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelBuddy.Application.Interfaces
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		public IActivityRepository ActivityRepository { get; }
+
+		public IApplicationUserRepository ApplicationUserRepository { get; }
+
+		public IItineraryRepository ItineraryRepository { get; }
+
+		public ITripRepository TripRepository { get; }
+
+	}
+}
---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿namespace TravelBuddy.Infrastructure
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly TravelBuddyDbContext dbContext;
+
+		public UnitOfWork() { }
+
+		public UnitOfWork(
+			TravelBuddyDbContext dbContext,
+			IActivityRepository activityRepository,
+			IItineraryRepository itineraryRepository,
+			ITripRepository tripRepository,
+			IApplicationUserRepository applicationUserRepository)
+		{
+			this.dbContext = dbContext;
+			this.ActivityRepository = activityRepository;
+			this.ItineraryRepository = itineraryRepository;
+			this.TripRepository = tripRepository;
+			this.ApplicationUserRepository = applicationUserRepository;
+		}
+
+		public IActivityRepository ActivityRepository { get; }
+
+		public IApplicationUserRepository ApplicationUserRepository { get; }
+
+		public IItineraryRepository ItineraryRepository { get; }
+
+		public ITripRepository TripRepository { get; }
+
+		public void Dispose()
+		{
+			dbContext.Dispose();
+		}
+	}
+}
