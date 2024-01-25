35d68c9a850b5ae007b815fcba091c8a6f68341f
Simeon Yordanov <sjordanov@code.bg>
2024-01-25 16:21:59 +0200

    added regions

--- TravelBuddy/backend/TravelBuddy/TravelBuddy/Extenstions/BuilderServices/ApplicationServices.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy/Extenstions/BuilderServices/ApplicationServices.cs
@@ -4,12 +4,22 @@
 	{
 		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
 		{
+			#region Repositories
+
 			services.AddScoped<IUnitOfWork, UnitOfWork>();
 			services.AddScoped<IActivityRepository, ActivityRepository>();
 			services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
 			services.AddScoped<IItineraryRepository, ItineraryRepository>();
 			services.AddScoped<ITripRepository, TripRepository>();
+
+			#endregion
+
+			#region Services
+
 			services.AddScoped<IAuthService, AuthService>();
+			services.AddScoped<ITripService, TripService>();
+
+			#endregion
 
 			return services;
 		}
