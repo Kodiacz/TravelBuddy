60b1912068417aa9c9cc0328c2d739155be9e9de
Simeon Yordanov <sjordanov@code.bg>
2024-01-25 16:23:22 +0200

    created and implemented trip service

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Interfaces/Services/ITripService.cs
@@ -0,0 +1,11 @@
+﻿using TravelBuddy.Application.Dtos.TripDtos;
+
+namespace TravelBuddy.Application.Interfaces.Services
+{
+	public interface ITripService
+	{
+		Task AddTrip(Trip trip);
+
+		Task<IEnumerable<GetTripDto>> GetUserTrips(Guid userId);
+	}
+}
---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/Services/TripService.cs
@@ -0,0 +1,19 @@
+﻿namespace TravelBuddy.Infrastructure.Services
+{
+	public class TripService : BaseService, ITripService
+	{
+		public TripService(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) { }
+
+		public Task AddTrip(Trip trip)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<GetTripDto>> GetUserTrips(Guid userId)
+		{
+			var trips = await this.UnitOfWork.TripRepository.GetAllAsReadOnlyAsync(userId);
+			var tripDtos = this.Mapper.Map<List<GetTripDto>>(trips);
+			return tripDtos;
+		}
+	}
+}
