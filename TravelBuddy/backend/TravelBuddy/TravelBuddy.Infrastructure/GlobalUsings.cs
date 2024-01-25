943ff6575302a6bb6f63dc9fd4de4cb16b06a617
Simeon Yordanov <sjordanov@code.bg>
2024-01-25 16:24:24 +0200

    added global usings

--- TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/GlobalUsings.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/GlobalUsings.cs
@@ -1,19 +1,25 @@
 ﻿global using System.Text;
-global using System.Linq.Expressions;
+global using System.Security.Claims;
 global using System.Security.Cryptography;
+global using System.Linq.Expressions;
+global using System.IdentityModel.Tokens.Jwt;
 
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.IdentityModel.Tokens;
 
 global using AutoMapper;
 
+global using TravelBuddy.Domain.Models;
 global using TravelBuddy.Domain.Entities;
 global using TravelBuddy.Application.Exceptions;
 global using TravelBuddy.Application.Interfaces;
 global using TravelBuddy.Application.Interfaces.Services;
 global using TravelBuddy.Application.Interfaces.Repository;
+global using TravelBuddy.Application.Dtos.TripDtos;
 global using TravelBuddy.Application.Dtos.ApplicationUserDto;
 global using TravelBuddy.Infrastructure.EntitiyConfigurations;
 global using static TravelBuddy.Infrastructure.Common.DatabaseConstants.ActivityConstants;
--- TravelBuddy/backend/TravelBuddy/TravelBuddy/GlobalUsings.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy/GlobalUsings.cs
@@ -15,8 +15,10 @@ global using System.ComponentModel.DataAnnotations;
 
 global using AutoMapper;
 
-global using TravelBuddy.Infrastructure;
 global using TravelBuddy.Domain.Entities;
+global using TravelBuddy.Infrastructure;
+global using TravelBuddy.Infrastructure.Repository;
+global using TravelBuddy.Infrastructure.Services;
 global using TravelBuddy.API.Filters;
 global using TravelBuddy.API.Controllers;
 global using TravelBuddy.API.CustomAttributes;
@@ -26,7 +28,6 @@ global using TravelBuddy.Application.Interfaces;
 global using TravelBuddy.Application.CustomAttributes;
 global using TravelBuddy.Application.Exceptions;
 global using TravelBuddy.Application.Interfaces.Services;
-global using TravelBuddy.Application.Dtos.ApplicationUserDto;
 global using TravelBuddy.Application.Interfaces.Repository;
-global using TravelBuddy.Infrastructure.Repository;
-global using TravelBuddy.Infrastructure.Services;
+global using TravelBuddy.Application.Dtos.TripDtos;
+global using TravelBuddy.Application.Dtos.ApplicationUserDto;
