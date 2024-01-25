b1e00b6bf50cc2d65dc47935eb291eb475decb64
Simeon Yordanov <sjordanov@code.bg>
2024-01-25 16:22:42 +0200

    crated and configured trip dto

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Dtos/TripDtos/GetTripDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelBuddy.Application.Dtos.TripDtos
+{
+	public class GetTripDto
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string Creator { get; set; } = null!;
+
+		public DateTime Date { get; set; }
+	}
+}
---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy/DtoProfiles/TripProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelBuddy.API.DtoProfiles
+{
+	public class TripProfile : Profile
+	{
+		public TripProfile()
+		{
+			CreateMap<Trip, GetTripDto>()
+				.ForMember(destinationMember => destinationMember.Creator, opt => opt.MapFrom(src => src.Creator.Email));
+		}
+
+	}
+}
