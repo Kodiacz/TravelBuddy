908e95cf7ddf22c6a6d298b2a163ae7222bfffa6
Simeon Yordanov <sjordanov@code.bg>
2024-02-01 13:20:38 +0200

    changed DateDeleted to accept null

--- TravelBuddy/backend/TravelBuddy/TravelBuddy.Domain/Entities/Itinerary.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Domain/Entities/Itinerary.cs
@@ -22,6 +22,6 @@ namespace TravelBuddy.Domain.Entities
 
 		public bool Deleted { get; set; } = false;
 
-		public DateTime DateDeleted { get; set; }
+		public DateTime? DateDeleted { get; set; }
 	}
 }
