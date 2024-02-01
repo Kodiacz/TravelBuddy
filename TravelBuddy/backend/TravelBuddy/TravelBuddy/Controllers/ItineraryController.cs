52b2b0e2e7737db0cf221c23b608bfdb2748aa13
Simeon Yordanov <sjordanov@code.bg>
2024-02-01 13:18:17 +0200

    created itinerary controller

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy/Controllers/ItineraryController.cs
@@ -0,0 +1,24 @@
+﻿namespace TravelBuddy.API.Controllers
+{
+	[ApiController]
+	[EnableCors("TravelBuddy")]
+	[Route("api/[controller]/[action]")]
+	public class ItineraryController : BaseController<AuthenticationController>
+	{
+		private readonly IItineraryService tripService;
+
+		public ItineraryController(IItineraryService tripService, IConfiguration configuration)
+		{
+			this.tripService = tripService;
+		}
+
+		[HttpGet]
+		[ActionName(nameof(GetTripItineraries))]
+		//[Authorize(Roles = "User")]
+		public async Task<ActionResult> GetTripItineraries(int tripId)
+		{
+			var trips = await this.tripService.GetTripItineraries(tripId);
+			return Ok(trips);
+		}
+	}
+}
