58006e12123bbf70f07eccd86c1340577619c265
Simeon Yordanov <sjordanov@code.bg>
2024-02-07 13:04:25 +0200

    added application services

--- TravelBuddy/backend/TravelBuddy/TravelBuddy/Extenstions/BuilderServices/ApplicationServices.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy/Extenstions/BuilderServices/ApplicationServices.cs
@@ -19,6 +19,7 @@
 			services.AddScoped<IAuthService, AuthService>();
 			services.AddScoped<ITripService, TripService>();
 			services.AddScoped<IItineraryService, ItineraryService>();
+			services.AddScoped<IActivityService, ActivityService>();
 
 			#endregion
 
