6c95e50e89dbd620db22117958c9c01bd4a84de9
Simeon Yordanov <sjordanov@code.bg>
2024-02-09 11:16:15 +0200

    renamed component

--- TravelBuddy/frontend/travel-buddy/src/components/ItineraryAccordion.tsx
+++ TravelBuddy/frontend/travel-buddy/src/components/ItineraryAccordion.tsx
@@ -1,71 +1,133 @@
-import { Icon, ListItem } from '@rneui/base';
-import React, { useState } from 'react';
-import { IItineraryAccordionProps } from '../types/propTypes';
-import ItineraryCard, { Activity } from './ItineraryCard';
-import { Image, StyleSheet } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { IItinerary } from '../types/applicationDbTypes';
 import { colors } from '../utils/colors';
+import Chevron from './Chevron';
+import Animated, {
+	Extrapolate,
+	interpolate,
+	measure,
+	runOnJS,
+	runOnUI,
+	useAnimatedRef,
+	useAnimatedStyle,
+	useDerivedValue,
+	useSharedValue,
+	withTiming,
+} from 'react-native-reanimated';
+import { Image } from 'react-native';
+import ActivityCard from './ActivityCard';
 
-const ItineraryAccordion = ({ itinerary }: IItineraryAccordionProps) => {
-	const [expanded, setExpanded] = useState<boolean>(false);
-	const itineraryDate = new Date(itinerary.date);
-	const icon = (
-		<Image
-			style={expanded ? { transform: [{ rotate: '360deg' }] } : {}}
-			source={require('../assets/pictures/accordion-arrow.png')}
-		></Image>
+type Props = {
+	itinerary: IItinerary;
+};
+
+const ItineraryAccordion = ({ itinerary }: Props) => {
+	const listRef = useAnimatedRef<Animated.View>();
+	const heightValue = useSharedValue(0);
+	const open = useSharedValue<boolean>(false);
+	const progress = useDerivedValue(() =>
+		open.value ? withTiming(1) : withTiming(0),
 	);
+	const test = 50;
+	const heightAnimationStyle = useAnimatedStyle(() => ({
+		height: interpolate(
+			progress.value,
+			[0, 1],
+			[0, heightValue.value],
+			Extrapolate.CLAMP,
+		),
+	}));
+
+	console.log(`${test * itinerary.activities.length}%`);
 
 	return (
-		<ListItem.Accordion
-			animation={{
-				type: 'timing',
-				duration: 250,
-			}}
-			containerStyle={{
-				...(expanded
-					? styles.openedAccordionContainer
-					: styles.closedAccordionContainer),
-				...styles.accordionContainer,
-			}}
-			icon={icon}
-			content={
-				<>
-					<ListItem.Content style={styles.accordionContent}>
-						<ListItem.Title>
-							{`${itinerary.name} - ${itineraryDate.toLocaleDateString()}`}
-						</ListItem.Title>
-					</ListItem.Content>
-				</>
-			}
-			isExpanded={expanded}
-			onPress={() => {
-				setExpanded(!expanded);
-			}}
-		>
-			<ItineraryCard itinerary={itinerary} />
-		</ListItem.Accordion>
+		<View style={styles.container}>
+			<Pressable
+				style={styles.titleContainer}
+				onPress={() => {
+					if (heightValue.value === 0) {
+						runOnUI(() => {
+							'worklet';
+							const measurement = measure(listRef);
+							heightValue.value = 48 * itinerary.activities.length;
+						})();
+					}
+					open.value = !open.value;
+				}}
+			>
+				<Text style={styles.textTitle}>{itinerary.name}</Text>
+				<Chevron progress={progress} />
+			</Pressable>
+			<Animated.View style={heightAnimationStyle}>
+				<Animated.View
+					ref={listRef}
+					style={styles.contentContainer}
+				>
+					<View style={styles.itineraryInfoContainer}>
+						<View>
+							<Text>
+								Itinerary for {new Date(itinerary.date).toLocaleDateString()}
+							</Text>
+						</View>
+						<Image
+							style={{ width: 20, height: 20, marginRight: '3%' }}
+							source={require('../assets/pictures/yellow-edit-pensil.png')}
+						/>
+					</View>
+					{itinerary.activities.map((a, i) => {
+						return (
+							<ActivityCard
+								activity={a}
+								key={i}
+							/>
+						);
+					})}
+				</Animated.View>
+			</Animated.View>
+		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	openedAccordionContainer: {
-		borderTopLeftRadius: 12.5,
-		borderTopRightRadius: 12.5,
-		height: 84,
-	},
-	closedAccordionContainer: {
-		borderRadius: 12.5,
-		height: 67,
-	},
-	accordionContainer: {
-		width: 365,
-		paddingRight: '8%',
-		alignSelf: 'center',
+	container: {
 		backgroundColor: colors.accent.absoluteApricot,
+		marginHorizontal: 10,
+		marginVertical: 10,
+		borderRadius: 12.5,
+		borderColor: '#F56B3',
+		overflow: 'hidden',
 	},
-	accordionContent: {
-		justifyContent: 'center',
+	titleContainer: {
+		padding: 20,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
 		alignItems: 'center',
+	},
+	textTitle: {
+		fontSize: 16,
+	},
+	contentContainer: {
+		borderTopRightRadius: 12.5,
+		borderTopLeftRadius: 12.5,
+		overflow: 'hidden',
+		backgroundColor: colors.white,
+		height: '100%',
+		// flex: 1,
+	},
+	content: {
+		padding: 20,
+		backgroundColor: '#D6E1F0',
+	},
+	textContent: {
+		fontSize: 14,
+	},
+	itineraryInfoContainer: {
+		paddingTop: 10,
+		paddingHorizontal: '1.5%',
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		backgroundColor: colors.white,
 	},
 });
 
