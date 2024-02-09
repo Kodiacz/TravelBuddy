f996d8408b15d448401ae2acebcbfddb095f4978
Simeon Yordanov <sjordanov@code.bg>
2024-02-09 11:15:44 +0200

    added new initial screen if-else logic

--- TravelBuddy/frontend/travel-buddy/App.tsx
+++ TravelBuddy/frontend/travel-buddy/App.tsx
@@ -5,7 +5,7 @@ import {
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider, connect } from 'react-redux';
 import { Dispatch } from 'redux';
-import { AppState, store } from './src/redux/store';
+import { AppReducers, persistedStore, reduxStore } from './src/redux/store';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { IUser, RootStackParamList } from './src/types/applicationDbTypes';
@@ -20,6 +20,11 @@ import Login from './src/screens/authentication/Login';
 import Main from './src/screens/Main';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { IAppProps } from './src/types/propTypes';
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { PersistGate } from 'redux-persist/integration/react';
+import { useSelector } from 'react-redux';
+import usePersistedUser from './src/custom-hooks/usePersistedUser';
 
 // Fix for 'atob' is not defined error in React Native
 if (!global.btoa) {
@@ -39,17 +44,43 @@ const authStore = createStore<IUser>({
 	cookieSecure: true,
 });
 
-const mapStateToProps = (state: AppState) => ({
-	count: state.counterReducer.count,
-	ps: state.tripReducer,
-});
+// const mapStateToProps = (state: AppReducers) => ({
+// 	count: state.counterReducer.count,
+// 	ps: state.tripReducer,
+// });
 
-const mapDispatchToProps = (dispatch: Dispatch): IAppProps => ({
-	increment: () => dispatch(incrementCount()),
-	decrement: () => dispatch(decrementCount()),
-});
+// const mapDispatchToProps = (dispatch: Dispatch): IAppProps => ({
+// 	increment: () => dispatch(incrementCount()),
+// 	decrement: () => dispatch(decrementCount()),
+// });
 
 export default function App() {
+	const [initialScreen, setInitialScreen] = useState<'Main' | 'Landing'>(
+		'Landing',
+	);
+
+	useEffect(() => {
+		const getUserInfo = async () => {
+			try {
+				const user = await AsyncStorage.getItem('persist:user').then((x) =>
+					JSON.parse(x!),
+				);
+
+				console.log('getUserInfo => user => ', user.data);
+
+				if (user.data) {
+					setInitialScreen('Main');
+				} else {
+					setInitialScreen('Landing');
+				}
+			} catch (error) {
+				setInitialScreen('Landing');
+			}
+		};
+
+		getUserInfo();
+	}, []);
+
 	return (
 		<GestureHandlerRootView style={{ flex: 1 }}>
 			<SafeAreaProvider>
@@ -59,32 +90,37 @@ export default function App() {
 					hidden={false}
 				/>
 				<AuthProvider store={authStore}>
-					<Provider store={store}>
-						<NavigationContainer>
-							<Stack.Navigator
-								initialRouteName="Landing"
-								screenOptions={{
-									header: () => null,
-								}}
-							>
-								<Stack.Screen
-									name="Landing"
-									component={Landing}
-								/>
-								<Stack.Screen
-									name="Register"
-									component={Register}
-								/>
-								<Stack.Screen
-									name="Login"
-									component={Login}
-								/>
-								<Stack.Screen
-									name="Main"
-									component={Main}
-								/>
-							</Stack.Navigator>
-						</NavigationContainer>
+					<Provider store={reduxStore}>
+						<PersistGate
+							loading={null}
+							persistor={persistedStore}
+						>
+							<NavigationContainer>
+								<Stack.Navigator
+									initialRouteName={initialScreen}
+									screenOptions={{
+										header: () => null,
+									}}
+								>
+									<Stack.Screen
+										name="Landing"
+										component={Landing}
+									/>
+									<Stack.Screen
+										name="Register"
+										component={Register}
+									/>
+									<Stack.Screen
+										name="Login"
+										component={Login}
+									/>
+									<Stack.Screen
+										name="Main"
+										component={Main}
+									/>
+								</Stack.Navigator>
+							</NavigationContainer>
+						</PersistGate>
 					</Provider>
 				</AuthProvider>
 			</SafeAreaProvider>
@@ -92,4 +128,4 @@ export default function App() {
 	);
 }
 
-connect(mapStateToProps, mapDispatchToProps)(App);
+// connect(mapStateToProps, mapDispatchToProps)(App);
