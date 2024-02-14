74f9c52914481c6189c4116a2dc8588140ac854e
Simeon Yordanov <sjordanov@code.bg>
2024-02-14 11:50:36 +0200

    created query for all Itineraries

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Queries/Itineraries/GetAllItinerariesQuery.cs
@@ -0,0 +1,18 @@
+﻿using TravelBuddy.Application.Models.Itineraries;
+
+namespace TravelBuddy.Application.Queries.Itineraries
+{
+	public class GetAllItinerariesQuery
+	{
+        public GetAllItinerariesQuery()
+        {
+            this.UserId = new Guid();
+        }
+
+		public string? SearchTerm { get; set; }
+
+		public ItinerarieSorting OrderBy { get; set; } 
+
+		public Guid UserId { get; set; }
+	}
+}	
