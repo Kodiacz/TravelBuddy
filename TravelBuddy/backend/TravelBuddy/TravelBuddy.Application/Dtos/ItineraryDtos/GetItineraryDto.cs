1b59b82623039302d895bf8aec9ecd5d279ecad7
Simeon Yordanov <sjordanov@code.bg>
2024-02-16 13:41:59 +0200

    added associated trip to GetItineraryDto

--- TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Dtos/ItineraryDtos/GetItineraryDto.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Dtos/ItineraryDtos/GetItineraryDto.cs
@@ -1,10 +1,13 @@
-﻿namespace TravelBuddy.Application.Dtos.ItineraryDtos
+﻿using TravelBuddy.Application.Dtos.TripDtos;
+
+namespace TravelBuddy.Application.Dtos.ItineraryDtos
 {
 	public class GetItineraryDto
 	{
         public GetItineraryDto()
         {
 			this.Activities = new List<GetActivityDto>();
+			this.Trip = new GetTripDto();
         }
 
         public int Id { get; set; }
@@ -14,5 +17,7 @@
 		public DateTime Date { get; set; }
 
 		public IEnumerable<GetActivityDto> Activities { get; set; }
+
+		public GetTripDto Trip { get; set; }
 	}
 }
