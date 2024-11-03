5217dd10640d81bd9c2f45435cf423dba7757ea3
Simeon Yordanov <SimeonYordanov@deltek.com>
2024-11-03 18:15:58 +0200

    overriden import

---
+++ TravelBuddy/frontend/travel-buddy/src/overriden-imports/react-native-responsive-screen.ts
@@ -0,0 +1,9 @@
+import {
+	heightPercentageToDP,
+	widthPercentageToDP,
+} from 'react-native-responsive-screen';
+
+const hp = heightPercentageToDP;
+const wp = widthPercentageToDP;
+
+export { hp, wp };
