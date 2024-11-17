f07f2abb9011ea227cc1a99383adf947cac5b058
Simeon Yordanov <SimeonYordanov@deltek.com>
2024-11-17 10:33:24 +0200

    extracted error messages into consts

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Messages.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelBuddy.Application
+{
+	public static class Messages
+	{
+		public static class Error
+		{
+			public const string INVALID_CREDENTIALS = "Invalid credentials. Please try again.";
+
+			public const string PASSWORDS_NOT_EQUAL = "Passwords are not the same";
+		}
+	}
+}
