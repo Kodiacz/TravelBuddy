bd35d67d0bd72c62c109fab3b95fa9ce2a11e5cb
Simeon Yordanov <SimeonYordanov@deltek.com>
2024-11-17 10:33:03 +0200

    moved method that generates hash in external class

---
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Application/Helpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TravelBuddy.Application
+{
+	public static class Helpers
+	{
+		public static class Encrypt
+		{
+			public static class Password
+			{
+				public static void CreateHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+				{
+					using (var hmac = new HMACSHA512())
+					{
+						passwordSalt = hmac.Key;
+						passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+					}
+				}
+			}
+		}
+	}
+}
--- TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/Services/AuthService.cs
+++ TravelBuddy/backend/TravelBuddy/TravelBuddy.Infrastructure/Services/AuthService.cs
@@ -1,64 +1,61 @@
-﻿namespace TravelBuddy.Infrastructure.Services
+﻿using TravelBuddy.API.Exceptions;
+
+namespace TravelBuddy.Infrastructure.Services;
+
+public class AuthService : BaseService, IAuthService
 {
-	public class AuthService : BaseService, IAuthService
+	public AuthService(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) { }
+
+	public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+	=> Helpers.Encrypt.Password.CreateHash(password, out passwordHash, out passwordSalt);
+
+
+	public async Task<GetApplicationUserDto> LoginUserAsync(LoginDto dto)
 	{
-		public AuthService(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) { }
+		var applicationUser = await base.UnitOfWork.ApplicationUserRepository.GetByEmailAsReadOnlyAsync(dto.Email);
+		if (applicationUser == null) throw new ApplicationArgumenNullException(Messages.Error.INVALID_CREDENTIALS);
 
-		public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+		if (!VerifyPasswordHash(applicationUser, dto))
+			throw new WrongPasswordException(Messages.Error.INVALID_CREDENTIALS);
+
+		return base.Mapper.Map<GetApplicationUserDto>(applicationUser);
+	}
+
+	public async Task<GetApplicationUserDto> RegisterUserAsync(CreateApplicationUserDto dto, byte[] passwordHash, byte[] passwordSalt)
+	{
+		if (!VerifyPasswordConfirmation(dto.Password, dto.ConfirmPassword))
+			throw new NotEqualPasswordsException(Messages.Error.PASSWORDS_NOT_EQUAL);
+
+		ApplicationUser applicationUser = base.Mapper.Map<ApplicationUser>(dto);
+		applicationUser.PasswordHash = Convert.ToBase64String(passwordHash);
+		applicationUser.PasswordSalt = passwordSalt;
+
+		await base.UnitOfWork.ApplicationUserRepository.AddAsync(applicationUser);
+		await base.UnitOfWork.SaveAsync();
+
+		var getUserDto = base.Mapper.Map<GetApplicationUserDto>(applicationUser);
+		return getUserDto;
+	}
+
+	private bool VerifyPasswordHash(ApplicationUser user, LoginDto dto)
+	{
+		using (var hmac = new HMACSHA512(user.PasswordSalt!))
 		{
-			using (var hmac = new HMACSHA512())
-			{
-				passwordSalt = hmac.Key;
-				passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-			}
+			var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password));
+			return computedHash.SequenceEqual(Convert.FromBase64String(user.PasswordHash));
 		}
+	}
 
-		public async Task<GetApplicationUserDto> LoginUserAsync(LoginDto dto)
-		{
-			var applicationUser = await base.UnitOfWork.ApplicationUserRepository.GetByEmailAsReadOnlyAsync(dto.Email);
-			if (applicationUser == null) throw new ArgumentNullException($"{nameof(applicationUser)} does not exist");
+	private bool VerifyPasswordConfirmation(string password, string confirmedPassword)
+	{
+		return password.Equals(confirmedPassword);
+	}
 
-			if (!VerifyPasswordHash(applicationUser, dto))
-				throw new InvalidOperationException("Password is incorect");
+	public JwtToken CreateToken(GetApplicationUserDto user, IConfiguration configuration)
+	{
+		JwtToken jwt = new();
 
-			return base.Mapper.Map<GetApplicationUserDto>(applicationUser);
-		}
-
-		public async Task<GetApplicationUserDto> RegisterUserAsync(CreateApplicationUserDto dto, byte[] passwordHash, byte[] passwordSalt)
-		{
-			ApplicationUser applicationUser = base.Mapper.Map<ApplicationUser>(dto);
-			applicationUser.PasswordHash = Convert.ToBase64String(passwordHash);
-			applicationUser.PasswordSalt = passwordSalt;
-
-			if (!VerifyPasswordConfirmation(dto.Password, dto.ConfirmPassword))
-				throw new InvalidOperationException("Passwords are not equal");
-
-			await base.UnitOfWork.ApplicationUserRepository.AddAsync(applicationUser);
-			await base.UnitOfWork.SaveAsync();
-
-			var getUserDto = base.Mapper.Map<GetApplicationUserDto>(applicationUser);
-			return getUserDto;
-		}
-
-		private bool VerifyPasswordHash(ApplicationUser user, LoginDto dto)
-		{
-			using (var hmac = new HMACSHA512(user.PasswordSalt!))
-			{
-				var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password));
-				return computedHash.SequenceEqual(Convert.FromBase64String(user.PasswordHash));
-			}
-		}
-
-		private bool VerifyPasswordConfirmation(string password, string confirmedPassword)
-		{
-			return password.Equals(confirmedPassword);
-		}
-
-		public JwtToken CreateToken(GetApplicationUserDto user, IConfiguration configuration)
-		{
-			JwtToken jwt = new();
-
-			List<Claim> claims = new()
+		List<Claim> claims = new()
 			{
 				new Claim(ClaimTypes.NameIdentifier, Convert.ToString(user.Id)!),
 				new Claim(ClaimTypes.Name, user.UserName!),
@@ -68,23 +65,22 @@
 				new Claim(ClaimTypes.Role, "User"),
 			};
 
-			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-				configuration.GetSection("JwtSettings:Secret").Value));
+		var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+			configuration.GetSection("JwtSettings:Secret").Value));
 
-			var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+		var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-			var token = new JwtSecurityToken(
-				claims: claims,
-				expires: DateTime.UtcNow.AddDays(1),
-				signingCredentials: cred);
+		var token = new JwtSecurityToken(
+			claims: claims,
+			expires: DateTime.UtcNow.AddDays(1),
+			signingCredentials: cred);
 
-			jwt.AccessToken = new JwtSecurityTokenHandler().WriteToken(token);
-			jwt.UserId = user.Id;
-			jwt.Email = user.Email;
-			jwt.ProfileImage = user.ProfileImage;
-			jwt.Role = "User";
+		jwt.AccessToken = new JwtSecurityTokenHandler().WriteToken(token);
+		jwt.UserId = user.Id;
+		jwt.Email = user.Email;
+		jwt.ProfileImage = user.ProfileImage;
+		jwt.Role = "User";
 
-			return jwt;
-		}
+		return jwt;
 	}
 }
